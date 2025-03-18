1e62c41db292bf1cc1048de61180b2a85038188e
DevanshuRastogi <125262981+DevanshuRastogi@users.noreply.github.com>
2025-03-18 10:45:37 +0530

    combine different reducers

---
+++ CodeEvolution/01/app05.js
@@ -0,0 +1,102 @@
+const redux = require("redux");
+
+const { createStore, bindActionCreators, combineReducers } = redux;
+const CAKE_ORDERED = "CAKE_ORDERED";
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
+const ICECREAM_ORDERED = "ICECREAM_ORDERED";
+const ICECREAM_RESTOCKED = "ICECREAM_RESTOCKED";
+function orderCakeAction() {
+  return {
+    type: CAKE_ORDERED,
+    // payload: 2,
+  };
+}
+function cakeRestocking(qnt) {
+  return {
+    type: "CAKE_RESTOCKED",
+    payload: qnt,
+  };
+}
+function orderIceCreamAction() {
+  return {
+    type: ICECREAM_ORDERED,
+    // payload: 2,
+  };
+}
+function IceCreamRestocking(qnt) {
+  return {
+    type: ICECREAM_RESTOCKED,
+    payload: qnt,
+  };
+}
+// (IntialState , action ) => newState  :logic of reducer
+
+const IntialState1 = {
+  numberOfCakes: 15,
+};
+const IntialState2 = {
+  numberOfIceCream: 10,
+};
+
+const cakeReducer = (state = IntialState1, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes - 1,
+      };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes + action.payload,
+      };
+
+      
+    default:
+      return state;
+  }
+};
+const iceCreamReducer = (state = IntialState2, action) => {
+  switch (action.type) {
+    case ICECREAM_ORDERED:
+      return {
+        ...state,
+        numberOfIceCream: state.numberOfIceCream - 1,
+      };
+    case ICECREAM_RESTOCKED:
+      return {
+        ...state,
+        numberOfIceCream: state.numberOfIceCream + action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+const rootReducer = combineReducers({
+    cakeReducer,
+    iceCreamReducer
+})
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+const unsubscribe = store.subscribe(() => console.log(store.getState()));
+
+const actions = bindActionCreators(
+  { orderCakeAction, cakeRestocking, IceCreamRestocking, orderIceCreamAction },
+  store.dispatch
+);
+
+actions.orderCakeAction();
+actions.orderCakeAction();
+actions.orderCakeAction();
+actions.cakeRestocking(3);
+actions.orderIceCreamAction();
+actions.orderIceCreamAction();
+actions.orderIceCreamAction();
+actions.orderIceCreamAction();
+actions.orderIceCreamAction();
+actions.IceCreamRestocking(5);
+
+unsubscribe();
+// store.dispatch(orderCakeAction()); // won't log
